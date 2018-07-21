17ee69b0bbe5cff9c1fb31d1a724a92d3e1738ed
Rahul Nayak <pragyes31@users.noreply.github.com>
2018-07-21 12:03:17 +0530

    to each it's own
    clicking one cal's button does not trigger another displayy.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const calc1 = document.querySelector(".calc1-outerBox");
-const calc2 = document.querySelector(".calc2-outerBox");
-const display = document.querySelectorAll(".result");
-const numberButtons = document.querySelectorAll(".number");
-const operationButtons = document.querySelectorAll(".ops");
-const clearContent = document.querySelectorAll(".clear");
-const equalButton = document.querySelectorAll(".equal");
-const negativeButton = document.querySelectorAll(".negative");
+const calc1Numbers = document.querySelectorAll(".calc1-outerBox .number");
+const calc2Numbers = document.querySelectorAll(".calc2-outerBox .number");
+const displayNum1 = document.querySelector(".calc1-outerBox .result");
+const displayNum2 = document.querySelector(".calc2-outerBox .result");
+const calc1ops = document.querySelectorAll(".calc1-outerBox .ops");
+const calc2ops = document.querySelectorAll(".calc2-outerBox .ops");
+
+function displayNum(display, eve) {
+  let firstNum = "";
+  let secondNum = "";
+
+  display.innerHTML += eve.target.innerHTML;
+}
+
+function recordOps(eve) {
+  let operator = "";
+  operator = eve.target.innerHTML;
+  displayNum;
+  console.log(operator);
+}
+
+calc1Numbers.forEach(e =>
+  e.addEventListener("click", eve => displayNum(displayNum1, eve))
+);
+calc2Numbers.forEach(e =>
+  e.addEventListener("click", eve => displayNum(displayNum2, eve))
+);
+calc1ops.forEach(e => e.addEventListener("click", eve => recordOps(eve)));
+calc2ops.forEach(e => e.addEventListener("click", eve => recordOps(eve)));
 
 let shouldLog = true;
 function log(...args) {
@@ -13,99 +34,3 @@ function log(...args) {
     console.log(...args);
   }
 }
-
-function test(e) {
-  console.log(e.target);
-  display[0].innerHTML += e.target.innerHTML;
-}
-
-calc1.addEventListener("click", e => test(e));
-calc2.addEventListener("click", e => test(e));
-
-// numberButtons.forEach(numButton =>
-//   numButton.addEventListener("click", e => displayNum(e))
-// );
-// operationButtons.forEach(opsButton =>
-//   opsButton.addEventListener("click", e => displayOps(e))
-// );
-
-// function flushContent() {
-//   display.innerHTML = "";
-//   number.firstNum = "";
-//   number.secondNum = "";
-//   operator = "";
-// }
-
-// function negativeSign(e, numberObj) {
-//   let negativeNumber = operator === "" ? "firstNum" : "secondNum";
-//   console.log(negativeNumber, e.target.innerHTML);
-//   if (numberObj[negativeNumber] === "") {
-//     console.log("reached here");
-//     numberObj[negativeNumber] += "-";
-//     display.innerHTML = "-";
-//   }
-// }
-
-// function displayNum(e, numberObj) {
-//   //console.log(e.target);
-//   let num = e.target.innerHTML;
-//   let numberClicked = operator === "" ? "firstNum" : "secondNum";
-//   if (num === ".") {
-//     numberObj[numberClicked] += numberObj[numberClicked].includes(".")
-//       ? ""
-//       : num;
-//   } else {
-//     numberObj[numberClicked] += num;
-//   }
-//   display.innerHTML = numberObj[numberClicked];
-// }
-
-// function displayOps(e) {
-//   if (number.secondNum !== "") {
-//     calculate();
-//   }
-//   operator = e.target.innerHTML;
-// }
-
-// function calculate() {
-//   if (
-//     number["firstNum"] !== "" &&
-//     number["secondNum"] !== "" &&
-//     number["secondNum"] !== "-"
-//   ) {
-//     let firstNumber = parseFloat(number["firstNum"]);
-//     let secondNumber = parseFloat(number["secondNum"]);
-//     let result;
-//     console.log(typeof firstNumber);
-//     console.log(typeof secondNumber);
-//     //result = `${firstNumber}${operator}${secondNumber}`;
-//     switch (operator) {
-//       case "+":
-//         result = firstNumber + secondNumber;
-//         break;
-//       case "-":
-//         result = firstNumber - secondNumber;
-//         break;
-//       case "*":
-//         result = firstNumber * secondNumber;
-//         break;
-//       case "/":
-//         result = firstNumber / secondNumber;
-//         break;
-//     }
-//     display.innerHTML = result;
-//     number.firstNum = result.toString();
-//     number.secondNum = "";
-//     operator = "";
-//   }
-// }
-
-// numberButtons.forEach(numButton =>
-//   numButton.addEventListener("click", e => displayNum(e))
-// );
-// clearContent.addEventListener("click", flushContent);
-// operationButtons.forEach(opsButton =>
-//   opsButton.addEventListener("click", e => displayOps(e))
-// );
-// equalButton.addEventListener("click", e => calculate(e));
-// negativeButton.addEventListener("click", e => negativeSign(e, number));
