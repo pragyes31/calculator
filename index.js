ad8d2ec1ea6ee51a3119a0c8f10a063b02c4e6c4
Rahul Nayak <pragyes31@users.noreply.github.com>
2018-07-30 18:36:40 +0530

    no global variables

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const calc1 = document.querySelector(".calc1-outerBox");
+const calc2 = document.querySelector(".calc2-outerBox");
 const calc1Numbers = document.querySelectorAll(".calc1-outerBox .number");
 const calc2Numbers = document.querySelectorAll(".calc2-outerBox .number");
 const calc1Result = document.querySelector(".calc1-outerBox .result");
@@ -18,117 +20,124 @@ function log(...args) {
   }
 }
 
-const calculator = {
-  operator: "",
-  firstNum: "",
-  secondNum: "",
-  recordOps: function(display, eve) {
-    if (calculator.secondNum !== "") {
-      calculator.calculate(display);
-    }
-    calculator.operator = eve.target.innerHTML;
-    console.log(calculator.operator);
-  },
-  displayNum: function(display, eve) {
-    let num = eve.target.innerHTML;
-    let numberClicked =
-      calculator["operator"] === "" ? "firstNum" : "secondNum";
-    if (num === ".") {
-      calculator[numberClicked] += calculator[numberClicked].includes(".")
-        ? ""
-        : num;
-    } else {
-      calculator[numberClicked] += eve.target.innerHTML;
-    }
-    console.log(calculator[numberClicked]);
-    console.log(numberClicked, calculator.firstNum);
-    console.log(calculator.operator, calculator.firstNum);
-    display.innerHTML = calculator[numberClicked];
-  },
-  clearDisplay: function(display) {
-    calculator.firstNum = "";
-    calculator.secondNum = "";
-    calculator.operator = "";
-    display.innerHTML = "";
-  },
-  negSign: function(display, eve) {
-    let negativeNumber =
-      calculator["operator"] === "" ? "firstNum" : "secondNum";
-    if (calculator[negativeNumber] === "") {
-      console.log("reached here");
-      calculator[negativeNumber] += "-";
-      display.innerHTML = "-";
-    }
-  },
-  calculate: function(display) {
-    console.log(
-      "entered calculate function",
-      calculator.operator,
-      calculator.secondNum
-    );
-    console.log(calculator.firstNum);
-    if (
-      calculator.firstNum !== "" &&
-      calculator.operator !== "" &&
-      calculator.secondNum !== ""
-    ) {
-      console.log("passed");
-      let firstNumber = parseFloat(calculator.firstNum);
-      let secondNumber = parseFloat(calculator.secondNum);
-      console.log(firstNumber, secondNumber);
-      let result;
-      // result = (`${firstNumber} ${operator} ${secondNumber}`);
-      switch (calculator.operator) {
-        case "+":
-          result = firstNumber + secondNumber;
-          break;
-        case "-":
-          result = firstNumber - secondNumber;
-          break;
-        case "*":
-          result = firstNumber * secondNumber;
-          break;
-        case "/":
-          result = firstNumber / secondNumber;
-          break;
+function createCalculator() {
+  const calculator = {
+    operator: "",
+    firstNum: "",
+    secondNum: "",
+    recordOps: function(display, eve) {
+      if (calculator.secondNum !== "") {
+        calculator.calculate(display);
       }
-      display.innerHTML = result;
-      calculator.firstNum = result.toString();
-      console.log(calculator.firstNum, calculator.secondNum);
+      calculator.operator = eve.target.innerHTML;
+      console.log(calculator.operator);
+    },
+    displayNum: function(display, eve) {
+      let num = eve.target.innerHTML;
+      let numberClicked =
+        calculator["operator"] === "" ? "firstNum" : "secondNum";
+      if (num === ".") {
+        calculator[numberClicked] += calculator[numberClicked].includes(".")
+          ? ""
+          : num;
+      } else {
+        calculator[numberClicked] += eve.target.innerHTML;
+      }
+      console.log(calculator[numberClicked]);
+      console.log(numberClicked, calculator.firstNum);
+      console.log(calculator.operator, calculator.firstNum);
+      display.innerHTML = calculator[numberClicked];
+    },
+    clearDisplay: function(display) {
+      calculator.firstNum = "";
       calculator.secondNum = "";
-      console.log(calculator.firstNum, calculator.secondNum);
       calculator.operator = "";
+      display.innerHTML = "";
+    },
+    negSign: function(display, eve) {
+      let negativeNumber =
+        calculator["operator"] === "" ? "firstNum" : "secondNum";
+      if (calculator[negativeNumber] === "") {
+        console.log("reached here");
+        calculator[negativeNumber] += "-";
+        display.innerHTML = "-";
+      }
+    },
+    calculate: function(display) {
+      console.log(
+        "entered calculate function",
+        calculator.operator,
+        calculator.secondNum
+      );
+      console.log(calculator.firstNum);
+      if (
+        calculator.firstNum !== "" &&
+        calculator.operator !== "" &&
+        calculator.secondNum !== ""
+      ) {
+        console.log("passed");
+        let firstNumber = parseFloat(calculator.firstNum);
+        let secondNumber = parseFloat(calculator.secondNum);
+        console.log(firstNumber, secondNumber);
+        let result;
+        // result = (`${firstNumber} ${operator} ${secondNumber}`);
+        switch (calculator.operator) {
+          case "+":
+            result = firstNumber + secondNumber;
+            break;
+          case "-":
+            result = firstNumber - secondNumber;
+            break;
+          case "*":
+            result = firstNumber * secondNumber;
+            break;
+          case "/":
+            result = firstNumber / secondNumber;
+            break;
+        }
+        display.innerHTML = result;
+        calculator.firstNum = result.toString();
+        console.log(calculator.firstNum, calculator.secondNum);
+        calculator.secondNum = "";
+        console.log(calculator.firstNum, calculator.secondNum);
+        calculator.operator = "";
+      }
     }
-  }
-};
+  };
+
+  return calculator;
+}
+
+const calculator1st = createCalculator();
+const calculator2nd = createCalculator();
 
 calc1Numbers.forEach(e =>
-  e.addEventListener("click", eve => calculator.displayNum(calc1Result, eve))
+  e.addEventListener("click", eve => calculator1st.displayNum(calc1Result, eve))
 );
 calc2Numbers.forEach(e =>
-  e.addEventListener("click", eve => calculator.displayNum(calc2Result, eve))
+  e.addEventListener("click", eve => calculator2nd.displayNum(calc2Result, eve))
 );
 calc1ClearButton.addEventListener("click", eve =>
-  calculator.clearDisplay(calc1Result)
+  calculator1st.clearDisplay(calc1Result)
 );
 calc2ClearButton.addEventListener("click", eve =>
-  calculator.clearDisplay(calc2Result)
+  calculator2nd.clearDisplay(calc2Result)
 );
 calc1ops.forEach(e =>
-  e.addEventListener("click", eve => calculator.recordOps(calc1Result, eve))
+  e.addEventListener("click", eve => calculator1st.recordOps(calc1Result, eve))
 );
 calc2ops.forEach(e =>
-  e.addEventListener("click", eve => calculator.recordOps(calc2Result, eve))
+  e.addEventListener("click", eve => calculator2nd.recordOps(calc2Result, eve))
 );
 calc1Equal.addEventListener("click", eve =>
-  calculator.calculate(calc1Result, eve)
+  calculator1st.calculate(calc1Result, eve)
 );
 calc2Equal.addEventListener("click", eve =>
-  calculator.calculate(calc2Result, eve)
+  calculator2nd.calculate(calc2Result, eve)
 );
 calc1Negative.addEventListener("click", eve =>
-  calculator.negSign(calc1Result, eve)
+  calculator1st.negSign(calc1Result, eve)
 );
 calc2Negative.addEventListener("click", eve =>
-  calculator.negSign(calc2Result, eve)
+  calculator2nd.negSign(calc2Result, eve)
 );
